ff22c6983b5c3d741a7dd5589745bf3bd026adc6
JobMurumba <murumbajob78@gmail.com>
2020-09-03 13:25:41 +0300

    fixed dom rendering before firebase ready

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import {connect} from 'react-redux'
-const Navbar = ()=>{   
+const Navbar = (props)=>{   
+    const {auth}= props
+    const links= auth.uid? (<SignedInLinks/>):(<SignedOutLinks/>)
      return(
        
         <nav className="nav-wrapper grey darken-3">
@@ -11,8 +13,7 @@ const Navbar = ()=>{
                 <Link to="/" className="brand-logo left">
                     Planner
                 </Link>
-            <SignedInLinks/>
-            <SignedOutLinks/>
+            {links}
             </div>
 
         </nav>  
@@ -21,7 +22,7 @@ const Navbar = ()=>{
 const mapStateToProps = state=>{
 console.log(state)
     return {
-
+        auth:state.firebase.auth
     }
 }
 export default connect(mapStateToProps)(Navbar);
--- src/index.js
+++ src/index.js
@@ -4,24 +4,45 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import {Provider} from 'react-redux';
+import {Provider,useSelector} from 'react-redux';
 import {createFirestoreInstance} from 'redux-firestore'
 import  firebase from './config/fbConfig'
-import {ReactReduxFirebaseProvider} from 'react-redux-firebase'
+import {ReactReduxFirebaseProvider, isLoaded} from 'react-redux-firebase'
 const rrfConfig = {
   userProfile:'users',
-  useFirestoreForProfile:true
+  useFirestoreForProfile:true,
+ 
 }
 const rrfProps = {
 firebase,
 config:rrfConfig,
+
 dispatch:store.dispatch,
 createFirestoreInstance,
+
 }
+
+
+
+
+
+
+
+
+function AuthIsLoaded ({children}){
+  const auth = useSelector(state=>state.firebase.auth)
+  if(!isLoaded(auth)) return <div>Loading...</div> 
+    return children
+}
+
+
+  
+  
+
 ReactDOM.render(
   <React.StrictMode><Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-    <App />
+    <AuthIsLoaded > <App /> </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
